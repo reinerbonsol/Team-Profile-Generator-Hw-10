f16c9b96dcf7117b5f0be77d1cd04fcf06b9951b
Reiner Bonsol <reinerbonsol93@gmail.com>
2020-05-11 16:46:59 -0400

    deleted extra js file and fixed new js file so inquirer would work

--- Develop/app.js
+++ Develop/app.js
@@ -1,35 +1,91 @@
-const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+const Manager = require("./lib/Manager");
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const employees = [];
+
+function addMember() {
+    inquirer.prompt([{
+        type: "input",
+        message: "Enter the name associated with team member",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select role associated with team member",
+        name: "role",
+        choices: [
+            "Intern",
+            "Engineer",
+            "Manager"
+        ],
+        
+    },
+    {
+        type: "input",
+        message: "Enter the ID associated with team member",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Enter the email associated with team member",
+        name: "email"
+    }])
+    .then(function({name, role, id, email}) {
+        let roleReturn = "";
+        if (role === "Intern") {
+            roleReturn = "school name";
+        } else if (role === "Engineer") {
+            roleReturn = "Github username";
+        } else {
+            roleReturn = "office phone number";
+        }
+        inquirer.prompt([{
+            type: "input",
+            message: `Enter ${roleReturn} associated with team member`,
+            name: "roleReturn"
+        },
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            name: "addTeamMember",
+            choices: [
+                "yes",
+                "no"
+            ],
+            
+        }])
+        .then(function({roleReturn, addTeamMember}) {
+            let newMember;
+            if (role === "Intern") {
+                newMember = new Intern(name, id, email, roleReturn);
+            } else if (role === "Engineer") {
+                newMember = new Engineer(name, id, email, roleReturn);
+            } else {
+                newMember = new Manager(name, id, email, roleReturn);
+            }
+            employees.push(newMember);
+            render(newMember)
+            .then(function() {
+                if (addTeamMember === "yes") {
+                    addMember();
+                } else {
+                    fs.appendFile(outputPath, function (err) {
+                        if (err) {
+                            console.log(err);
+                        };
+                    });
+                    console.log("Team Members added and created in team.html");
+                }
+            });
+        });
+    });
+};
+
+addMember();
